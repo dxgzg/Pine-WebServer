4833d1c02433fc009cbba8ff3a7a42191217047e
dxgzg <1481592124@qq.com>
2020-12-11 19:43:54 +0800

    del

--- src/Socket.cpp
+++
@@ -1,9 +0,0 @@
-#include "Socket.h"
-#include <sys/types.h>      
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-bool Socket::sock(int family){
-    this->listen_fd = socket(family,SOCK_STREAM,IPPROTO_TCP);
-}
